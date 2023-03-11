f2ae84f57fd6a4f007a2a632178e49566f1cc21f
Tanbeer Jubaer <85180135+oitik@users.noreply.github.com>
2023-03-11 16:44:12 +0600

    Update Pilling Up!.py
    added

--- Pilling Up!.py
+++ Pilling Up!.py
@@ -1,1 +1,30 @@
+def solve(data):
+    n = data[0]
+    lis = data[1]
+    stk = [10000000000]
+    left = 0
+    right = n-1
+    for i in range(n):
+        if lis[left] >= lis[right]:
+            stk.append(lis[left])
+            if stk[i+1] > stk[i]:
+                return 'No'
+            left += 1
+        elif lis[left] < lis[right]:
+            stk.append(lis[right])
+            if stk[i+1] > stk[i]:
+                return 'No'
+            right -= 1
+    return 'Yes'
+
+
+t = int(input())
+in_list = []
+for i in range(t):
+    n = int(input())
+    x = [int(x) for x in input().split()]
+    x = (n, x)
+    in_list.append(x)
+for val in in_list:
+    print(solve(val))
 
